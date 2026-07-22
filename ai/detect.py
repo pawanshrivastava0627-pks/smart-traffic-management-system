085b059425ef837f228b0c55af13472481f5dc93
Pawan Kumar Shrivastava <pawan.shrivastava0627@gmail.com>
2026-07-23 00:16:28 +0530

    feat: implement vehicle detection and tracking using YOLOv8

--- ai/detect.py
+++ ai/detect.py
@@ -1,6 +1,39 @@
 from ultralytics import YOLO
 import cv2
 
+# Load YOLO model
 model = YOLO("yolov8n.pt")
 
-print("YOLO model loaded successfully!")
+# Load video
+video_path = "input/traffic.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Check if video opened successfully
+if not cap.isOpened():
+    print("Error: Unable to open video.")
+    exit()
+
+print("Video loaded successfully!")
+frame_count = 0
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    results = model.track(
+        frame,
+        persist=True,
+        verbose=False
+    )
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("Traffic Video", annotated_frame)
+
+    if cv2.waitKey(30) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
